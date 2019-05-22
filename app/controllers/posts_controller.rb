534dcdf239edf8d15b2b01bfa10c9041388a9bbf
Leila Weiss <leila.weiss@powerhrg.com>
2019-05-22 13:29:55 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,13 +21,11 @@ class PostsController < ApplicationController
 private
 
   def post_params
-    params
-      .require(:post)
-      .permit(
-        :title,
-        :content,
-        category_ids: [],
-        categories_attributes: [:name]
+  def post_params
+    params.require(:post).permit(
+      :title, :content,
+      category_ids:[],
+      categories_attributes: [:name]
       )
   end
 end
